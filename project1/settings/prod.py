b3101e93191f1b938d164957ac7991fa70e2987b
VenkatSaiGS <venkatsaigs@VenkatSai>
2025-12-07 01:57:29 +0530

    Ready for deploy

--- project1/settings/prod.py
+++ project1/settings/prod.py
@@ -2,21 +2,26 @@ from .common import *
 import os
 import dj_database_url
 
+# Secret Key
 SECRET_KEY = os.environ["SECRET_KEY"]
 
+# Debug
 DEBUG = False
 
-# Allowed hosts
+# Allowed Hosts
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 render_hostname = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if render_hostname:
     ALLOWED_HOSTS.append(render_hostname)
 
 # Database
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL environment variable is required!")
+
 DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600)
+    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 }
-DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
 
 # Redis / Celery
 REDIS_URL = os.environ.get("REDIS_URL")
@@ -46,13 +51,13 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
-# Static & media
+# Static & Media
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 
-# Security headers for production
+# Security headers
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
